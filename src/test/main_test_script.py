afa97e81d2cc89bee335a4f7c91edf7791106eac
Lazar <lazar.manic@novelic.com>
2022-06-05 23:23:19 +0200

    Changed url paths to being relative, changed private members
    text and word members of Search() are changed to be public for easier test
    setups

--- src/test/main_test_script.py
+++ src/test/main_test_script.py
@@ -1,21 +1,36 @@
+import sys
+import os
+from pathlib import Path
+
+# issues in importing parent directories as modules 
+parent_subdirectory = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir)) 
+parent_directory = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+
+
+# append path directories as modules
+sys.path.append(parent_directory)
+sys.path.append(parent_subdirectory)
+
 ###################### Test set 1 #########################
 print("Test set 1: Tekst: Coffea arabica, Chromosome 1c i paterni: ATGCATG, TCTCTCTA, TTCACTACTCTCA")
 
 words = ['ATGCATG', 'TCTCTCTA', 'TTCACTACTCTCA']
-file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
+
+
+file = "../../data/chr1.fna" # enter relative path to the data
+#file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
 
 from src import search as s
 from src.business_logic import heuristics as hh
 
 # Alhabet in FASTA file
 # TODO double check
-alphabet = 'ACGT'
+alphabet = 'ACGTN'
 
 for word in words:
     # TODO: h1, h2, h1 + h2
-    program_4 = s.Search(file=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
+    program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
     program_4.do_magic()
-
 
 
 
@@ -25,11 +40,11 @@ print("Test set 2: Tekst: Mus pahari chromosome X, i paterni: ATGATG, CTCTCTA, T
 
 # TODO: add correct filepath
 words = ['ATGATG', 'CTCTCTA', 'TCACTACTCTCA']
-file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
+#file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
 
 for word in words:
     # TODO: h1, h2, h1 + h2
-    program_4 = s.Search(file=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
+    program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
     program_4.do_magic()
 
 
@@ -41,9 +56,9 @@ print("Test set 3: Genom po slobodnom izboru iz NIH baze i proizvoljna 3 paterna
 
 # TODO: change words and add correct filepath
 words = ['ATGCATG', 'TCTCTCTA', 'TTCACTACTCTCA']
-file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
+#file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
 
 for word in words:
     # TODO: h1, h2, h1 + h2
-    program_4 = s.Search(file=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
+    program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
     program_4.do_magic()
