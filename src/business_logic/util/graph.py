5167bad4d387e2f549fd261f624bf390647db58b
Tamara Sretković <tsretkovic@microsoft.com>
2022-06-06 14:26:44 +0200

    Add more x tics

--- src/business_logic/util/graph.py
+++ src/business_logic/util/graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 class Graph:
     """Class to drive graph for visualisation"""
@@ -18,15 +19,17 @@ class Graph:
         data_dict = { 'Heuristic': heuristics, 'Word lenght': words_len, 'Time': times, 'Memory': memories }
         df = pd.DataFrame(data_dict)
         print(df)
-
         fig, ax = plt.subplots(1, 2, figsize=(5, 2.7), layout='constrained')
-
         df1_h1 = df[df['Heuristic'] == 'Goodsuffix']
         df1_h1 = df1_h1.sort_values(by=['Memory'], ascending=True)
         print(df1_h1)
         df1_h2 = df[df['Heuristic'] == 'Badcharacter + Goodsuffix']
         df1_h2 = df1_h2.sort_values(by=['Memory'], ascending=True)
         print(df1_h2)
+        ax[0].grid()
+        ax[0].set_xticks(np.arange(min(df['Word lenght']), max(df['Word lenght'])+1, 1.0))
+        ax[1].set_xticks(np.arange(min(df['Word lenght']), max(df['Word lenght'])+1, 1.0))
+
         ax[0].bar(df1_h1['Word lenght'] - 0.25, df1_h1['Memory'], label='Memory Goodsuffix', width = 0.5)  # Plot some data on the axes.
         ax[0].bar(df1_h2['Word lenght'] + 0.25, df1_h2['Memory'], label='Memory Badcharacter + Goodsuffix', width = 0.5)  # Plot some data on the axes.
 
@@ -42,6 +45,7 @@ class Graph:
         df2_h2 = df[df['Heuristic'] == 'Badcharacter + Goodsuffix']
         df2_h2 = df2_h2.sort_values(by=['Time'], ascending=True)
         print(df2_h2)
+        ax[1].grid()
         ax[1].bar(df2_h1['Word lenght'] - 0.25, df2_h1['Time'], label='Time Goodsuffix', width = 0.5)  # Plot some data on the axes.
         ax[1].bar(df2_h2['Word lenght'] + 0.25, df2_h2['Time'], label='Time Badcharacter + Goodsuffix', width = 0.5)  # Plot some data on the axes.
 
