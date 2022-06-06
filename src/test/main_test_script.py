ad8b4649c1cd2c373c49d9b35b33912ffb0c8493
Tamara Sretković <tsretkovic@microsoft.com>
2022-06-06 19:23:18 +0200

    Create graph at the end of main script

--- src/test/main_test_script.py
+++ src/test/main_test_script.py
@@ -11,26 +11,24 @@ parent_directory = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 
 sys.path.append(parent_directory)
 sys.path.append(parent_subdirectory)
 
+programs = []
 ###################### Test set 1 #########################
 print("Test set 1: Tekst: Coffea arabica, Chromosome 1c i paterni: ATGCATG, TCTCTCTA, TTCACTACTCTCA")
 
 words = ['ATGCATG', 'TCTCTCTA', 'TTCACTACTCTCA']
-file = "../../data/chr1.fna" # enter relative path to the data
-#file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
+# file = "../data/chr1.fna" # enter relative path to the data
+file = 'C:/Users/tsretkovic/Desktop/skola/GI/PROJEKAT/GI-projekat/src/data/chr1.fna'
 
 from src import search as s
 from src.business_logic import heuristics as hh
 
 # Alhabet in FASTA file
-# TODO double check
 alphabet = 'ACGTN'
 
 for word in words:
-    # TODO: h1, h2, h1 + h2
-    program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
-    program_4.do_magic()
-
-
+    # TODO add all heuristics
+    programs.append(s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)]))
+    programs[len(programs) - 1].do_magic()
 
 # ###################### Test set 2 #########################
 
@@ -41,9 +39,9 @@ for word in words:
 # #file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
 
 # for word in words:
-#     # TODO: h1, h2, h1 + h2
-#     program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
-#     program_4.do_magic()
+    # TODO add all heuristics
+    # programs.append(s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)]))
+    # programs[len(programs) - 1].do_magic()
 
 
 
@@ -57,6 +55,10 @@ for word in words:
 # #file = "C:\Users\tsretkovic\Desktop\skola\GI\PROJEKAT\chr1.fna.gz"
 
 # for word in words:
-#     # TODO: h1, h2, h1 + h2
-#     program_4 = s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)])
-#     program_4.do_magic()
+    # TODO add all heuristics
+    # programs.append(s.Search(file_path=file, word=word, heuristics=[hh.Badcharacter(word, alphabet), hh.Goodsuffix(word)]))
+    # programs[len(programs) - 1].do_magic()
+
+
+import business_logic.util.graph as g
+g.Graph().Drive(programs)
